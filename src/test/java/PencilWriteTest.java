c79838c931a48cf857572a0fd9e1eb641d9b598f
Dylan Barrow <barrowdylan9@gmail.com>
2019-03-03 11:36:17 -0500

    Created BeforeEach setup method in PencilWriteTest to setup paper and pencil for each test

--- src/test/java/PencilWriteTest.java
+++ src/test/java/PencilWriteTest.java
@@ -1,22 +1,30 @@
 import com.barrow.PencilDurability.Pencil;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PencilWriteTest {
 
+    String paper;
+    Pencil pencil;
+
+    @BeforeEach
+    public void setup() {
+        paper = "";
+        pencil = new Pencil();
+    }
+
     @Test
     public void PencilWriteToEmptyPaper() {
-        String paper = "";
-        Pencil pencil = new Pencil();
         paper = pencil.write(paper, "Pencils are used for writing");
         assertEquals("Pencils are used for writing", paper);
     }
 
     @Test
     public void PencilWriteToPaperWithExistingContents() {
-        String paper = "Pencils are: ";
-        Pencil pencil = new Pencil();
+        paper = "Pencils are: ";
         paper = pencil.write(paper, "used for writing");
         assertEquals("Pencils are: used for writing", paper);
     }
